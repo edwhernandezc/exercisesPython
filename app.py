b6745e8c7fd0d82d8be7b4c06c5ee04800c867d6
Edwin Hernandez Cabrera <info@encefalo.co>
2019-05-19 21:19:19 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+lista = [1, 2.5, 'DevCode', [5,6] ,4]
+for element in lista:
+    print(element)
+
+
+n1 = int(input("Ingrese primer numero: "))
+n2 = int(input("Ingrese segundo numero: "))
+if n1 < n2:
+	for i in range(n1+1,n2):
+		print (i)
+
+
+pow(2, 3)
